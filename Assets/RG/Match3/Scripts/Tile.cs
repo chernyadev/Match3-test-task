dc2c378c3a94c628d246adefd54e9d71b304a379
nchernyadev <nikita.chernyadev@gmail.com>
2022-04-04 22:16:16 +0300

    Refactor Tile component to use data structures to store configurable parameters

--- Assets/RG/Match3/Scripts/Tile.cs
+++ Assets/RG/Match3/Scripts/Tile.cs
@@ -1,27 +1,15 @@
 using System;
 using System.Threading.Tasks;
 using DG.Tweening;
+using RG.Match3.Data;
 using UnityEngine;
 
 namespace RG.Match3 {
     public class Tile : MonoBehaviour {
         public event Action<Tile> onTileClicked;
 
-        [SerializeField]
-        private int id;
-
-        [SerializeField]
-        private float size = 1f;
-
-        [Header("Animations")]
-        [SerializeField]
-        private float removeAnimationDuration = 0.2f;
-        [SerializeField]
-        private int removeAnimationVibration = 10;
-        [SerializeField]
-        private float removeAnimationElasticity = 0.1f;
-        [SerializeField]
-        private float moveAnimationDuration = 0.2f;
+        private TileData data;
+        private TileAnimationData animationData;
 
         private Vector2Int boardCoords = Vector2Int.zero;
         public Vector2Int BoardCoords {
@@ -31,27 +19,33 @@ namespace RG.Match3 {
                 transform.localPosition = BoardCoordsToWorldSpacePosition(boardCoords);
             }
         }
-        public int Id => id;
+        public int Id => data.id;
 
         private void OnMouseDown() {
             onTileClicked?.Invoke(this);
         }
 
+        public void Init(TileData data, TileAnimationData animationData) {
+            this.data = data;
+            this.animationData = animationData;
+        }
+
         public void Remove() {
             transform
-                .DOPunchScale(Vector3.one, removeAnimationDuration, removeAnimationVibration, removeAnimationElasticity)
+                .DOPunchScale(Vector3.one, animationData.removeDuration, animationData.removeVibration,
+                    animationData.removeElasticity)
                 .OnComplete(() => Destroy(gameObject));
         }
 
         public Task Move(Vector2Int targetCoords) {
             boardCoords = targetCoords;
-            return transform.DOLocalMove(BoardCoordsToWorldSpacePosition(targetCoords), moveAnimationDuration)
+            return transform.DOLocalMove(BoardCoordsToWorldSpacePosition(targetCoords), animationData.moveDuration)
                 .SetEase(Ease.OutBounce)
                 .AsyncWaitForCompletion();
         }
 
         private Vector3 BoardCoordsToWorldSpacePosition(Vector2 coord) {
-            return new Vector2(coord.x, coord.y) * size + Vector2.one * (size * 0.5f);
+            return new Vector2(coord.x, coord.y) * data.size + Vector2.one * (data.size * 0.5f);
         }
     }
 }
